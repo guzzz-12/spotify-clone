1af54ae04d08a67f954958f5e232979c2ffd383d
unknown <jegq.dev.projects@gmail.com>
2023-06-23 22:51:45 -0400

    Modal de login/signup

---
+++ components/AuthModal.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { useSupabaseClient, useSessionContext } from "@supabase/auth-helpers-react";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import GenericModal from "./GenericModal"
+import useAuthModal from "@/hooks/useAuthModal";
+import { useEffect } from "react";
+
+const AuthModal = () => {
+  const router = useRouter();
+  const supabase = useSupabaseClient();
+  const {session} = useSessionContext();
+
+  const {isOpen, onOpenChange} = useAuthModal();
+
+  // Cerrar el modal al iniciar sesión
+  useEffect(() => {
+    if (session) {
+      router.refresh();
+      onOpenChange(false);
+    }
+  }, [session, router, onOpenChange]);
+
+  return (
+    <GenericModal
+      title="Welcome back"
+      description="Login to your account"
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+    >
+      <Auth
+        supabaseClient={supabase}
+        theme="dark"
+        magicLink
+        providers={["github", "google"]}
+        queryParams={{
+          prompt: "consent"
+        }}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: "#404040",
+                brandAccent: "#141414"
+              }
+            }
+          }
+        }}
+      />
+    </GenericModal>
+  )
+};
+
+export default AuthModal;
