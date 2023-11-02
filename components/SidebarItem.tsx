44eefba90e2befeb0a1690ae51dd47e6b5e79d2f
unknown <jegq.dev.projects@gmail.com>
2023-11-02 10:57:07 -0400

    Corrección de import not found

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { twMerge } from "tailwind-merge";
-import { RouteItem } from "./Sidebar";
+import { RouteItem } from "./SidebarContent";
 
 interface Props {
   routeItem: RouteItem;
